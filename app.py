7d1b075e50f26c81950c84f536ba28e32349359e
slm37102 <slm37102@hotmail.com>
2022-03-09 23:01:01 +0800

    refine code

--- app.py
+++ app.py
@@ -4,9 +4,6 @@ import pydicom
 from pydicom.pixel_data_handlers.util import apply_voi_lut
 
 header = st.container()
-dataset = st.container()
-feature = st.container()
-model_training = st.container()
 
 def get_x(r):
     return r['filepath']
@@ -20,32 +17,23 @@ def dicom2png(file):
     if dicom.PhotometricInterpretation == "MONOCHROME1":
         data = np.amax(data) - data
     data = data - np.min(data)
-    if np.max(data) == 0:
-        return None, None, None 
     data = data / np.max(data)
     data = (data * 255).astype(np.uint8)
-    if not data.any():
-        return None, None, None
-        
     im = Image.fromarray(data)
     return im
 
 class WrongFileType(ValueError):
     pass
 
-# @st.cache()
-# def load_model(path: str = 'export.pkl'):
-#     """Retrieves the trained model and maps it to the CPU by default,
-#     can also specify GPU here."""
-#     learn = load_learner(path)
-#     return learn
-
-path = 'export.pkl'
 image_path = 'Image-13.png'
-learn = load_learner(path)
+learn = load_learner('export.pkl')
 
 with header:
     st.header('Pog Prediction')
+    # option = st.selectbox(
+    #     'How would you like to be contacted?',
+    #     ('Email', 'Home phone', 'Mobile phone')
+    # )
     dicom_bytes = st.file_uploader("Upload DICOM file")
 
     if not dicom_bytes:
