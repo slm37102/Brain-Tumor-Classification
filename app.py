784e973c60a3b055dcdb43f16852eaeb5fd777d9
slm37102 <slm37102@hotmail.com>
2022-03-09 21:20:57 +0800

    basic function

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-import pandas as pd
+import streamlit as st
 from fastai.vision.all import *
-import time
+import pydicom
+from pydicom.pixel_data_handlers.util import apply_voi_lut
 
 import pathlib
 temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
+
+header = st.container()
+dataset = st.container()
+feature = st.container()
+model_training = st.container()
 
 def get_x(r):
     return r['filepath']
@@ -12,7 +18,46 @@ def get_x(r):
 def get_y(r):
     return r['MGMT_value']
 
-learn = load_learner('export.pkl')
-start_time = time.time()
-print(learn.predict('Image-13.png'))
-print(time.time() - start_time)
+def dicom2png(file):
+    dicom = pydicom.read_file(file, force=True)
+    data = apply_voi_lut(dicom.pixel_array, dicom)
+    if dicom.PhotometricInterpretation == "MONOCHROME1":
+        data = np.amax(data) - data
+    data = data - np.min(data)
+    if np.max(data) == 0:
+        return None, None, None 
+    data = data / np.max(data)
+    data = (data * 255).astype(np.uint8)
+    if not data.any():
+        return None, None, None
+        
+    im = Image.fromarray(data)
+    return im
+
+class WrongFileType(ValueError):
+    pass
+
+# @st.cache()
+# def load_model(path: str = 'export.pkl'):
+#     """Retrieves the trained model and maps it to the CPU by default,
+#     can also specify GPU here."""
+#     learn = load_learner(path)
+#     return learn
+
+path = 'export.pkl'
+image_path = 'Image-13.png'
+learn = load_learner(path)
+
+with header:
+    st.header('Pog Prediction')
+    dicom_bytes = st.file_uploader("Upload DICOM file")
+
+    if not dicom_bytes:
+        raise st.stop()  
+    try:
+        png = dicom2png(dicom_bytes)
+    except:
+        st.write(WrongFileType("Does not appear to be a DICOM file"))
+        raise st.stop()
+    st.image(png)
+    st.text(learn.predict(image_path))
